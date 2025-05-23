269b9c31f8a3d745c808de7607484363304aab6e
Francois Bonneau <francois.bonneau@geode-solutions.com>
2025-05-23 16:44:59 +0200

    Initial commit

---
+++ bindings/python/mylib.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+
+from opengeode_mymodule_py_mylib import *
+MyModuleMyLibLibrary.initialize()
---
+++ include/mylib/common.hpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+ */
+
+#pragma once
+
+#include <geode/basic/common.hpp>
+#include <geode/basic/library.hpp>
+#include <mylib/opengeode_mymodule_mylib_export.hpp>
+
+namespace mymodule
+{
+    OPENGEODE_LIBRARY( opengeode_mymodule_mylib_api, MyModuleMyLib );
+} // namespace mymodule
