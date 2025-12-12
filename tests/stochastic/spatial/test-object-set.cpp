f8414711d6dc00071c0d8fb1fc6e6e97df9f7950
Francois Bonneau <francois.bonneau@geode-solutions.com>
2025-12-12 18:21:47 +0100

    fix test

--- tests/stochastic/spatial/test-object-set.cpp
+++ tests/stochastic/spatial/test-object-set.cpp
@@ -118,31 +118,31 @@ namespace
         // ici u0 est changé? doije retourner l indice?
 
         OPENGEODE_ASSERT( set.nb_fixed_objects() == 2 );
-        OPENGEODE_ASSERT( set.nb_fixed_objectsree() == 1 );
+        OPENGEODE_ASSERT( set.nb_fixed_objects() == 1 );
         OPENGEODE_ASSERT( f0 == 0 && f1 == 1 );
 
         // Add 2 free objects
         auto u1 = set.add_free_object( geode::Point2D{ { 2., 2. } } );
         auto u2 = set.add_free_object( geode::Point2D{ { 3., 3. } } );
 
-        OPENGEODE_ASSERT( set.nb_fixed() == 2 );
-        OPENGEODE_ASSERT( set.nb_free() == 3 );
+        OPENGEODE_ASSERT( set.nb_fixed_objects() == 2 );
+        OPENGEODE_ASSERT( set.nb_free_objects() == 3 );
         OPENGEODE_ASSERT( u1 == 3 && u2 == 4 );
 
         // Remove first fixed object
         set.remove_object( 0 );
 
         // Invariant
-        OPENGEODE_ASSERT( set.nb_fixed() == 1 );
-        OPENGEODE_ASSERT( set.nb_free() == 3 );
-        OPENGEODE_ASSERT( set.size() == 4 );
+        OPENGEODE_ASSERT( set.nb_fixed_objects() == 1 );
+        OPENGEODE_ASSERT( set.nb_free_objects() == 3 );
+        OPENGEODE_ASSERT( set.nb_objects() == 4 );
 
         // Remove a free object
         set.remove_free_object( 2 );
 
-        OPENGEODE_ASSERT( set.nb_fixed() == 1 );
-        OPENGEODE_ASSERT( set.nb_free() == 2 );
-        OPENGEODE_ASSERT( set.size() == 3 );
+        OPENGEODE_ASSERT( set.nb_fixed_objects() == 1 );
+        OPENGEODE_ASSERT( set.nb_free_objects() == 2 );
+        OPENGEODE_ASSERT( set.nb_objects() == 3 );
     }
 } // namespace
 
